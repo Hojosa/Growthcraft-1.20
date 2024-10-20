57a083a384ed26236fd14c60cbeb9f2de0134a07
Fox <mccloud.fox21@gmail.com>
2024-10-20 15:16:42 +0200

    make logs strippable

--- src/main/java/growthcraft/lib/block/GrowthcraftLogBlock.java
+++ src/main/java/growthcraft/lib/block/GrowthcraftLogBlock.java
@@ -1,13 +1,20 @@
 package growthcraft.lib.block;
 
+import org.jetbrains.annotations.Nullable;
+
+import growthcraft.apples.init.GrowthcraftApplesBlocks;
+import growthcraft.cellar.init.GrowthcraftCellarBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ToolAction;
 
 public class GrowthcraftLogBlock extends RotatedPillarBlock {
     public GrowthcraftLogBlock() {
@@ -38,6 +45,27 @@ public class GrowthcraftLogBlock extends RotatedPillarBlock {
     @Override
     public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
         return 5;
+    }
+    
+    //since vanilla uses a proteced map, to specify the which logs turn into which stripped version, this is the best way i found. 
+    //we could also do sub classes or our own map to match logs&stripped logs, but for 4 blocks, this should be enough
+    @Override
+    public @Nullable BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction toolAction, boolean simulate) {
+    	if (context.getItemInHand().getItem() instanceof AxeItem) {
+    		if(state.is(GrowthcraftApplesBlocks.APPLE_WOOD_LOG.get())) {
+    			return GrowthcraftApplesBlocks.APPLE_WOOD_LOG_STRIPPED.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+    		}
+    		if(state.is(GrowthcraftApplesBlocks.APPLE_WOOD.get())) {
+    			return GrowthcraftApplesBlocks.APPLE_WOOD_STRIPPED.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+    		}
+    		if(state.is(GrowthcraftCellarBlocks.CORK_WOOD_LOG.get())) {
+    			return GrowthcraftCellarBlocks.CORK_WOOD_LOG_STRIPPED.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+    		}
+    		if(state.is(GrowthcraftCellarBlocks.CORK_WOOD.get())) {
+    			return GrowthcraftCellarBlocks.CORK_WOOD_STRIPPED.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+    		}
+    	}
+    	return super.getToolModifiedState(state, context, toolAction, simulate);
     }
 
 }
