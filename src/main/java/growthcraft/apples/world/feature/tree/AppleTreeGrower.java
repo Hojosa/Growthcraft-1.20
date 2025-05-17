44ff934beaa15564b19a3c914eb99404a5c9c43b
Fox <mccloud.fox21@gmail.com>
2025-05-17 12:56:33 +0200

    move features into one class, add back the old apple tree, make the new bigger one a sub-chance

--- src/main/java/growthcraft/apples/world/feature/tree/AppleTreeGrower.java
+++ src/main/java/growthcraft/apples/world/feature/tree/AppleTreeGrower.java
@@ -1,17 +1,18 @@
 package growthcraft.apples.world.feature.tree;
 
-import growthcraft.apples.world.GrowthcraftApplesConfiguredFeatures;
+import org.jetbrains.annotations.Nullable;
+
+import growthcraft.apples.world.GrowthcraftApplesFeatures;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import org.jetbrains.annotations.Nullable;
 
 public class AppleTreeGrower extends AbstractTreeGrower {
 
     @Nullable
     @Override
     protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(RandomSource randomSource, boolean pHasFlowers) {
-        return GrowthcraftApplesConfiguredFeatures.APPLE_TREE_KEY;
+        return GrowthcraftApplesFeatures.Configured.TREES_APPLE;
     }
 }
